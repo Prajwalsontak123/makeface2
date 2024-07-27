bd0b2db5792f74f50f201067dab04be8d1c2485e
propratik123 <pratikpetkar231@gmail.com>
2024-07-27 14:47:49 +0530

    ok

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -43,7 +43,7 @@ class HomeScreen extends StatelessWidget {
             onPressed: () {
               Navigator.push(
                 context,
-                MaterialPageRoute(builder: (context) => HomeChatScreen()),
+                MaterialPageRoute(builder: (context) => HomeChat()),
               );
             },
           ),
